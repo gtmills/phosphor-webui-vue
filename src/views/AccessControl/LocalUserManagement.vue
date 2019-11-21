c95505992ebe044f2e6c578f8e3c911736f584d8
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-11-21 10:41:20 -0800

    Add carbon table to local user page
    - Using static data to local user table

--- src/views/AccessControl/LocalUserManagement.vue
+++ src/views/AccessControl/LocalUserManagement.vue
@@ -1,3 +1,39 @@
 <template>
-  <h1>Local user management</h1>
+  <div class="bx--grid">
+    <div class="bx--row">
+      <div class="bx--col-sm-12">
+        <h1>Local user management</h1>
+      </div>
+    </div>
+    <div class="bx--row">
+      <div class="bx--col-lg-8 bx--col-md-10">
+        <cv-data-table :columns="columns" :data="data"></cv-data-table>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { CvDataTable } from "@carbon/vue";
+export default {
+  name: "local-users",
+  components: {
+    CvDataTable
+  },
+  data() {
+    return {
+      columns: ["Username", "Privilege", "Status"],
+      data: [
+        ["root", "Admin", "Enabled"],
+        ["user1", "User", "Enabled"]
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  margin-bottom: 2rem;
+}
+</style>
