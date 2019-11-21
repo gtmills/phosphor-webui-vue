fd9c3a17a2f0891c1a8179acb7def87523fc7a49
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-11-21 12:29:40 -0800

    Add user role accordion component
    - Add unstyled accordion component and static
      role table data

--- src/views/AccessControl/LocalUserManagement.vue
+++ src/views/AccessControl/LocalUserManagement.vue
@@ -7,20 +7,32 @@
     </div>
     <div class="bx--row">
       <div class="bx--col-lg-8 bx--col-md-10">
-        <cv-data-table :columns="columns" :data="data">
+        <cv-data-table :columns="userTable.columns" :data="userTable.data">
           <template slot="actions">
             <cv-button
               :icon="icon.settings"
               :kind="'ghost'"
               @click="onClickSettings"
             >
-              Account Policy Settings
+              Account policy settings
             </cv-button>
             <cv-button :icon="icon.add" @click="onClickAdd">
               Add users
             </cv-button>
           </template>
         </cv-data-table>
+      </div>
+    </div>
+    <div class="bx--row">
+      <div class="bx--col-lg-6 bx--col-md-8">
+        <cv-accordion>
+          <cv-accordion-item :open="accordion.open">
+            <template slot="title">View privilege role descriptions</template>
+            <template slot="content">
+              <role-table></role-table>
+            </template>
+          </cv-accordion-item>
+        </cv-accordion>
       </div>
     </div>
     <cv-modal :visible="modal.settingsOpen" @modal-hidden="modalClosed">
@@ -37,24 +49,30 @@
 </template>
 
 <script>
-import { CvDataTable, CvButton, CvModal } from "@carbon/vue";
+import { CvDataTable, CvButton, CvModal, CvAccordion } from "@carbon/vue";
 import Settings16 from "@carbon/icons-vue/es/settings/16";
 import Add16 from "@carbon/icons-vue/es/add/16";
+
+import LocalUserMangementRoleTable from "./LocalUserManagementRoleTable";
 
 export default {
   name: "local-users",
   components: {
     CvDataTable,
     CvButton,
-    CvModal
+    CvModal,
+    CvAccordion,
+    roleTable: LocalUserMangementRoleTable
   },
   data() {
     return {
-      columns: ["Username", "Privilege", "Status"],
-      data: [
-        ["root", "Admin", "Enabled"],
-        ["user1", "User", "Enabled"]
-      ],
+      userTable: {
+        columns: ["Username", "Privilege", "Status"],
+        data: [
+          ["root", "Admin", "Enabled"],
+          ["user1", "User", "Enabled"]
+        ]
+      },
       icon: {
         add: Add16,
         settings: Settings16
@@ -62,6 +80,9 @@ export default {
       modal: {
         addUserOpen: false,
         settingsOpen: false
+      },
+      accordion: {
+        expanded: false
       }
     };
   },
@@ -83,5 +104,14 @@ export default {
 <style lang="scss" scoped>
 h1 {
   margin-bottom: 2rem;
+}
+.cv-data-table {
+  margin-bottom: 2rem;
+}
+.bx--accordion__item {
+  border: none;
+}
+.bx--accordion__content {
+  padding: 0;
 }
 </style>
---
+++ src/views/AccessControl/LocalUserManagementRoleTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <cv-data-table :columns="columns">
+    <template slot="data">
+      <cv-data-table-row
+        v-for="(row, rowIndex) in data"
+        :key="`${rowIndex}`"
+        :value="`${rowIndex}`"
+      >
+        <cv-data-table-cell
+          v-for="(cell, cellIndex) in row"
+          :key="`${cellIndex}`"
+          :value="`${cellIndex}`"
+        >
+          <span v-if="cell === true"><Checkmark16 /></span>
+          <span v-else>{{ cell }}</span>
+        </cv-data-table-cell>
+      </cv-data-table-row>
+    </template>
+  </cv-data-table>
+</template>
+
+<script>
+import Checkmark16 from "@carbon/icons-vue/es/checkmark/16";
+
+export default {
+  components: {
+    Checkmark16
+  },
+  data() {
+    return {
+      columns: ["", "Admin", "Operator", "User", "Callback"],
+      data: [
+        ["Configure components managed by this service", true, "", "", ""],
+        ["Configure manager resources", true, "", "", ""],
+        ["Update password for current user account", true, "", "", ""],
+        ["Configure users and their accounts", true, "", "", ""],
+        ["Log in to the service and read resources", true, true, true, true],
+        ["IPMI access point", true, true, true, true],
+        ["Redfish access point", true, true, true, true],
+        ["SSH access point", true, true, true, true],
+        ["WebUI access point", true, true, true, true]
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+td:not(:first-child) {
+  text-align: center;
+  vertical-align: middle;
+}
+</style>
