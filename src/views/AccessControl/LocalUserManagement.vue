1742e43f3aa146d74b741e2ee27563502b3ae816
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-11-21 11:30:08 -0800

    Add table actions
    - Added table action modals with dummy content

--- src/views/AccessControl/LocalUserManagement.vue
+++ src/views/AccessControl/LocalUserManagement.vue
@@ -1,24 +1,52 @@
 <template>
   <div class="bx--grid">
     <div class="bx--row">
-      <div class="bx--col-sm-12">
+      <div class="bx--col-lg-8">
         <h1>Local user management</h1>
       </div>
     </div>
     <div class="bx--row">
       <div class="bx--col-lg-8 bx--col-md-10">
-        <cv-data-table :columns="columns" :data="data"></cv-data-table>
+        <cv-data-table :columns="columns" :data="data">
+          <template slot="actions">
+            <cv-button
+              :icon="icon.settings"
+              :kind="'ghost'"
+              @click="onClickSettings"
+            >
+              Account Policy Settings
+            </cv-button>
+            <cv-button :icon="icon.add" @click="onClickAdd">
+              Add users
+            </cv-button>
+          </template>
+        </cv-data-table>
       </div>
     </div>
+    <cv-modal :visible="modal.settingsOpen" @modal-hidden="modalClosed">
+      <template slot="title">Account policy settings</template>
+      <template slot="secondary-button">Cancel</template>
+      <template slot="primary-button">Save</template>
+    </cv-modal>
+    <cv-modal :visible="modal.addUserOpen" @modal-hidden="modalClosed">
+      <template slot="title">Add user</template>
+      <template slot="secondary-button">Cancel</template>
+      <template slot="primary-button">Save</template>
+    </cv-modal>
   </div>
 </template>
 
 <script>
-import { CvDataTable } from "@carbon/vue";
+import { CvDataTable, CvButton, CvModal } from "@carbon/vue";
+import Settings16 from "@carbon/icons-vue/es/settings/16";
+import Add16 from "@carbon/icons-vue/es/add/16";
+
 export default {
   name: "local-users",
   components: {
-    CvDataTable
+    CvDataTable,
+    CvButton,
+    CvModal
   },
   data() {
     return {
@@ -26,8 +54,28 @@ export default {
       data: [
         ["root", "Admin", "Enabled"],
         ["user1", "User", "Enabled"]
-      ]
+      ],
+      icon: {
+        add: Add16,
+        settings: Settings16
+      },
+      modal: {
+        addUserOpen: false,
+        settingsOpen: false
+      }
     };
+  },
+  methods: {
+    onClickAdd() {
+      this.modal.addUserOpen = true;
+    },
+    onClickSettings() {
+      this.modal.settingsOpen = true;
+    },
+    modalClosed() {
+      this.modal.settingsOpen = false;
+      this.modal.addUserOpen = false;
+    }
   }
 };
 </script>
