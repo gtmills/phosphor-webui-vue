d7b0c7aa10cadfefff459b936554eb63cc3d5c99
Derick Montague <derick.montague@ibm.com>
2019-11-21 12:03:52 -0600

    Add Axios library to application
    - Add a prototype method named $http to vue prototype
    
    Signed-off-by: Derick Montague <derick.montague@ibm.com>

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Axios from "axios";
 import CarbonComponentsVue from "@carbon/vue";
 import "@/assets/styles/index.scss";
 
+Vue.prototype.$http = Axios;
 Vue.use(CarbonComponentsVue);
 
 new Vue({
