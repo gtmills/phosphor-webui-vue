0a1d370877acee5012d43fb4f330cc0283f1aa32
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2019-11-26 13:41:44 -0800

    Add inline table actions
    Table actions triggering dummy modals.
    
    Signed-off-by: Yoshie Muranaka <yoshiemuranaka@gmail.com>

--- src/views/AccessControl/LocalUserManagement.vue
+++ src/views/AccessControl/LocalUserManagement.vue
@@ -15,11 +15,23 @@
     </b-row>
     <b-row>
       <b-col lg="8" md="10">
-        <b-table
-          bordered
-          head-variant="dark"
-          :items="userTable.items"
-        ></b-table>
+        <b-table bordered head-variant="dark" :items="userTable.items">
+          <template v-slot:head(actions)="data"></template>
+          <template v-slot:cell(actions)="data">
+            <b-button
+              :disabled="!data.value.edit"
+              v-b-modal.modal-user-settings
+            >
+              <Edit20 />
+            </b-button>
+            <b-button
+              :disabled="!data.value.delete"
+              v-b-modal.modal-user-delete
+            >
+              <TrashCan20 />
+            </b-button>
+          </template>
+        </b-table>
       </b-col>
     </b-row>
     <b-row>
@@ -35,30 +47,50 @@
     <!-- Modals -->
     <b-modal id="modal-add-user" title="Add user">
       <template v-slot:modal-footer="{ ok, cancel, hide }">
-        <b-button size="sm" variant="secondary" @click="cancel()"
-          >Cancel</b-button
-        >
-        <b-button size="sm" variant="primary" @click="ok()">Add user</b-button>
+        <b-button size="sm" variant="secondary">Cancel</b-button>
+        <b-button size="sm" variant="primary">Add user</b-button>
       </template>
     </b-modal>
-    <b-modal id="modal-settings" title="Account policy settings"> </b-modal>
+    <b-modal id="modal-settings" title="Account policy settings"></b-modal>
+    <b-modal id="modal-user-delete" title="Delete user"></b-modal>
+    <b-modal id="modal-user-settings" title="User settings"></b-modal>
   </b-container>
 </template>
 
 <script>
 import LocalUserManagementRoleTable from "./LocalUserMangementRoleTable";
+import TrashCan20 from "@carbon/icons-vue/es/trash-can/20";
+import Edit20 from "@carbon/icons-vue/es/edit/20";
 
 export default {
   name: "local-users",
   components: {
+    TrashCan20,
+    Edit20,
     roleTable: LocalUserManagementRoleTable
   },
   data() {
     return {
       userTable: {
         items: [
-          { username: "root", privilege: "Admin", status: "Enabled" },
-          { username: "user", privilege: "User", status: "Enabled" }
+          {
+            username: "root",
+            privilege: "Admin",
+            status: "Enabled",
+            actions: {
+              edit: true,
+              delete: false
+            }
+          },
+          {
+            username: "user",
+            privilege: "User",
+            status: "Enabled",
+            actions: {
+              edit: true,
+              delete: true
+            }
+          }
         ]
       }
     };
