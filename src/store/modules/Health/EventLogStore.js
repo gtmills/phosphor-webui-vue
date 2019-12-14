c0bf1301830d39a2f6c24cb95bf60521be842322
Dixsie Wolmers <dixsiew@gmail.com>
2019-12-14 12:05:05 -0600

    Remove unused properties in event log store

--- src/store/modules/Health/EventLogStore.js
+++ src/store/modules/Health/EventLogStore.js
@@ -23,6 +23,21 @@ const EventLogStore = {
           const eventLog = JSON.stringify(data.data);
           const eventLogContent = JSON.parse(eventLog);
           let eventLogData = [];
+          const severityFlags = {
+            low: false,
+            medium: false,
+            high: false
+          };
+          const severityToPriorityMap = {
+            Emergency: 'High',
+            Alert: 'High',
+            Critical: 'High',
+            Error: 'High',
+            Warning: 'Medium',
+            Notice: 'Low',
+            Debug: 'Low',
+            Informational: 'Low'
+          };
 
           for (let key in eventLogContent) {
             let severityCode = '';
@@ -30,28 +45,13 @@ const EventLogStore = {
             let relatedItems = [];
             let eventID = 'None';
             let description = 'None';
-            const severityFlags = {
-              low: false,
-              medium: false,
-              high: false
-            };
-            const SEVERITY_TO_PRIORITY_MAP = {
-              Emergency: 'High',
-              Alert: 'High',
-              Critical: 'High',
-              Error: 'High',
-              Warning: 'Medium',
-              Notice: 'Low',
-              Debug: 'Low',
-              Informational: 'Low'
-            };
 
             if (
               eventLogContent.hasOwnProperty(key) &&
               eventLogContent[key].hasOwnProperty('Id')
             ) {
               severityCode = eventLogContent[key].Severity.split('.').pop();
-              priority = SEVERITY_TO_PRIORITY_MAP[severityCode];
+              priority = severityToPriorityMap[severityCode];
               severityFlags[priority.toLowerCase()] = true;
 
               if (eventLogContent[key].hasOwnProperty(['Associations'])) {
@@ -72,18 +72,9 @@ const EventLogStore = {
                 Object.assign(
                   {
                     path: key,
-                    copied: false,
                     priority: priority,
                     severity_code: severityCode,
                     severity_flags: severityFlags,
-                    additional_data: eventLogContent[key].AdditionalData.join(
-                      '\n'
-                    ),
-                    type: eventLogContent[key].Message,
-                    selected: false,
-                    meta: false,
-                    confirm: false,
-                    related_items: relatedItems,
                     eventID: eventID,
                     description: description,
                     logId: '#' + eventLogContent[key].Id,
