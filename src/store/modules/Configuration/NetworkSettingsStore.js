7153ed8c9113206137545cacf5ed74f2ffa05e7a
Dixsie Wolmers <dixsiew@gmail.com>
2020-01-03 19:55:46 -0500

    Refactor network settings store

--- src/store/modules/Configuration/NetworkSettingsStore.js
+++ src/store/modules/Configuration/NetworkSettingsStore.js
@@ -34,70 +34,21 @@ const NetworkSettingsStore = {
       api
         .get('/xyz/openbmc_project/network/enumerate')
         .then(({ data }) => {
-          const networkData = JSON.stringify(data.data);
-          const networkDataContent = JSON.parse(networkData);
-          let parsedNetworkData = {
-            interface_ids: [],
-            interfaces: {},
-            ip_addresses: { ipv4: [], ipv6: [] }
-          };
-          let interfaceId = '',
-            keyParts = [],
-            interfaceHash = '',
-            interfaceType = '';
-          for (let key in networkDataContent) {
-            if (key.match(/network\/eth\d+(_\d+)?$/gi)) {
-              interfaceId = key.split('/').pop();
-              if (parsedNetworkData.interface_ids.indexOf(interfaceId) == -1) {
-                parsedNetworkData.interface_ids.push(interfaceId);
-                parsedNetworkData.interfaces[interfaceId] = {
-                  interfaceIname: '',
-                  DomainName: '',
-                  MACAddress: '',
-                  Nameservers: [],
-                  DHCPEnabled: 0,
-                  ipv4: { ids: [], values: [] },
-                  ipv6: { ids: [], values: [] }
-                };
-                parsedNetworkData.interfaces[interfaceId].MACAddress =
-                  networkDataContent[key].MACAddress;
-                parsedNetworkData.interfaces[
-                  interfaceId
-                ].DomainName = networkDataContent[key].DomainName.join(' ');
-                parsedNetworkData.interfaces[interfaceId].Nameservers =
-                  networkDataContent[key].Nameservers;
-                parsedNetworkData.interfaces[interfaceId].DHCPEnabled =
-                  networkDataContent[key].DHCPEnabled;
-              }
-            } else if (
-              key.match(/network\/eth\d+(_\d+)?\/ipv[4|6]\/[a-z0-9]+$/gi)
+          const networkData = data.data;
+          const ipAddresses = [];
+          const interfaceId = /eth[0-9]/;
+          for (let key in networkData) {
+            if (key.includes('ipv4')) {
+              ipAddresses.push(networkData[key].Address);
+            }
+            if (
+              key.match(interfaceId) &&
+              networkData[key].MACAddress !== undefined
             ) {
-              keyParts = key.split('/');
-              interfaceHash = keyParts.pop();
-              interfaceType = keyParts.pop();
-              interfaceId = keyParts.pop();
-              if (
-                parsedNetworkData.interfaces[interfaceId][
-                  interfaceType
-                ].ids.indexOf(interfaceHash) == -1
-              ) {
-                parsedNetworkData.interfaces[interfaceId][
-                  interfaceType
-                ].ids.push(interfaceHash);
-                parsedNetworkData.interfaces[interfaceId][
-                  interfaceType
-                ].values.push(networkDataContent[key]);
-                parsedNetworkData.ip_addresses[interfaceType].push(
-                  networkDataContent[key]['Address']
-                );
-              }
+              commit('setMacAddress', networkData[key].MACAddress);
             }
           }
-          commit(
-            'setMacAddress',
-            networkDataContent['/xyz/openbmc_project/network/eth0'].MACAddress
-          );
-          commit('setIpAddress', parsedNetworkData.ip_addresses.ipv4);
+          commit('setIpAddress', ipAddresses);
         })
         .catch(error => {
           console.log('Network Data:', error);
